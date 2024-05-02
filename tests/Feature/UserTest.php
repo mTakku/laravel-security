3f072fe0505014e585c422c52126d210cceee54b
Takku <farelvivo06@gmail.com>
2024-05-02 22:24:00 +0700

    added guard

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -56,4 +56,21 @@ class UserTest extends TestCase
             ->get("/users/current")
             ->assertSeeText("Hello Farel");
     }
+
+    public function testTokenGuard()
+    {
+        $this->seed([UserSeeder::class]);
+
+        $this->get("/api/users/current", [
+            "Accept" => "application/json"
+        ])
+            ->assertStatus(401);
+
+        $this->get("/api/users/current", [
+            "Accept" => "application/json",
+            "API-Key" => "secret"
+        ])
+            ->assertSeeText("Hello Farel");
+
+    }
 }
